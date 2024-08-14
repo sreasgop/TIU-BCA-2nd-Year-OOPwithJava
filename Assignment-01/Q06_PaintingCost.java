29e80882970228ad838a3fc042e880a2a5156648
sreasgop <sreasgop@gmail.com>
2024-08-14 10:07:13 +0530

    Q06_PaintingCost.java added

---
+++ Assignment-01/Q06_PaintingCost.java
@@ -0,0 +1,25 @@
+// 6.	To determine the area of the walls of a rectangular room and hence the cost of its painting on the basis of charge per square unit. 
+
+import java.util.Scanner;
+
+public class Q06_PaintingCost {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nEnter height of wall: ");
+        float wallHeight = sc.nextFloat();
+
+        System.out.print("Enter length of the wall: ");
+        float wallLength = sc.nextFloat();
+
+        System.out.print("Enter cost of paint (per Sq. Unit): ");
+        float cost = sc.nextFloat();
+
+        float wallArea = wallHeight * wallLength;
+        float totalCost = wallArea * cost;
+
+        System.out.printf("Total Area of Wall: %.2f unit sq.\nTotal Cost to Paint: %.2f\n", wallArea, totalCost);
+
+    }
+}
